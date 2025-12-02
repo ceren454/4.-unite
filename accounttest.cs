7c735fa7338deaa77883d88ccce82baa345ec950
ceren454 <cerebtoy@gmail.com>
2025-12-02 23:42:40 +0300

    Create accounttest.cs

---
+++ accounttest.cs
@@ -0,0 +1,19 @@
+using System; 
+
+class AccountTest
+{
+    static void Main()
+    {
+        Account myAccount = new Account();
+
+        Console.WriteLine($"Initial name is: {myAccount.GetName()}");
+
+        Console.Write("Enter the name: "); 
+
+        string theName = Console.ReadLine(); 
+
+        myAccount.SetName(theName); 
+
+        Console.WriteLine($"myAccount's name is: {myAccount.GetName()}");
+    }
+}
